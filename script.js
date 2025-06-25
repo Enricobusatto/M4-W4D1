8d8c114c1a83aa8f5bf3249509203e9fc8df0db2
Enricobusatto <enricobusatto75@gmail.com>
2025-06-25 15:22:57 +0200

    first save

---
+++ script.js
@@ -0,0 +1,35 @@
+async function fetchProducts() {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODViMWFiZjk2OGRlNTAwMTU1MmEzZTgiLCJpYXQiOjE3NTA4MDE1ODYsImV4cCI6MTc1MjAxMTE4Nn0.xhkKzesrQbsg05eG0q_X5718-KDBF6O110MAHGmTcKo"
+            }
+        });
+    }
+    catch (e) {
+        console.error("Error fetching products:", e);
+    }
+}
+fetchProducts()
+console.log(fetchProducts());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch("https://striveschool-api.herokuapp.com/api/product/"),
+//     headers {
+//     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODViMWFiZjk2OGRlNTAwMTU1MmEzZTgiLCJpYXQiOjE3NTA4MDE1ODYsImV4cCI6MTc1MjAxMTE4Nn0.xhkKzesrQbsg05eG0q_X5718-KDBF6O110MAHGmTcKo"
+// }
